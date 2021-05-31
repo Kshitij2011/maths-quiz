500f3c72fdc9c6212267777c2fdad91b5d579d8f
Kshitij2011 <77619958+Kshitij2011@users.noreply.github.com>
2021-05-31 22:13:33 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -4,27 +4,69 @@ player2_name = localStorage.getItem("player_2_name");
 player1_score = 0;
 player2_score = 0;
 
-    function send(){
-        get_number = document.getElementById("sum").value;
-        console.log("sum");
-    
-        charAt1 = sum.charAt(2);
-        console.log(charAt1);
-         length_divide_2 = Math.floor(sum.length/2);
-          charAt2 = number.charAt(length_divide_2);
-           console.log(charAt2);
-    
-          length_minus_1 = number.length - 1;
-             charAt3 = number.charAt(length_minus_1);
-              console.log(charAt3);
+document.getElementById("player_1_name").innerHTML = player1_name + " : ";
+document.getElementById("player_2_name").innerHTML = player2_name + " : ";
 
-              remove_charAt1 = number.replace(charAt1,"X");
-              console.log(remove_charAt3);
-    
-                   question_word = "<h4 id='sum_display'>Q."+remove_charAt1+"</h4>";
-                    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
-                     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-                      row = question_word + input_box + check_button;
-                       document.getElementById("output").innerHTML = row;
-                        document.getElementById("sum").value = "";
+document.getElementById("player_1_score").innerHTML = player1_score ;
+document.getElementById("player_2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+
+
+    function send(){
+        number1 = document.getElementById("sum").value;
+	    number2 = document.getElementById("sum_2").value;
+	    actual_answer = parseInt(number1) * parseInt(number2);
+	    console.log(actual_answer);
+
+        question_word = "<h4>" +number1+ "X" +number2+ "</h4>";
+        input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+        check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+        row = question_word + input_box + check_button;
+        document.getElementById("output").innerHTML = row;
+        document.getElementById("sum").value = "";
+        document.getElementById("sum_2").value = "";
     }
+
+    function check()
+{
+	get_answer = document.getElementById("input_check_box").value;
+	if(get_answer == actual_answer)	
+	{
+		if(answer_turn == "player1")
+		{
+			player1_score = player1_score +1;
+		    document.getElementById("player1_score").innerHTML = player1_score;
+		}
+		else 
+		{
+			player2_score = player2_score +1;
+		    document.getElementById("player2_score").innerHTML = player2_score;
+		}
+	}
+	
+	if(question_turn == "player1")
+	{
+		question_turn = "player2"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+	}
+	else 
+	{
+		question_turn = "player1"
+		document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+	}
+
+	if(answer_turn == "player1")
+	{
+		answer_turn = "player2"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+	}
+	else 
+	{
+		answer_turn = "player1"
+		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+	}
+
+    document.getElementById("output").innerHTML = "";
+}
